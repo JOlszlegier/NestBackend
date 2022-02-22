7d6edf17eb4d6e17f06d064542166a3d29a75c50
kolszlegier41 <kolszlegier41@gmail.com>
2022-02-22 13:50:02 +0100

    test<users>: POST e2e test

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,16 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
 import { UsersModule } from '../src/users/users.module';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import User from '../src/users/user.entity';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
-  const mockUsersRepository = {
-    find: jest.fn(),
+  const mockUsers = [{ id: 1, name: 'Kuba' }];
+  const mockNewUser = {
+    name: 'Bob',
+    email: 'test@test.com',
+    password: 'typicalpassword',
   };
+  const mockUsersRepository = {
+    find: jest.fn().mockResolvedValue(mockUsers),
+    create: jest.fn().mockImplementation((dto) => dto),
+    save: jest.fn().mockImplementation((user) => Promise.resolve({ user })),
+  };
+
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [UsersModule],
@@ -23,10 +31,27 @@ describe('UserController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/users (GET)', () => {
     return request(app.getHttpServer())
       .get('/users/all')
       .expect(200)
-      .expect(Array);
+      .expect(mockUsers);
+  });
+
+  it('/users (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/users')
+      .send(mockNewUser)
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .then((response) =>
+        expect(response.body).toEqual({
+          email: 'test@test.com',
+          income: 0,
+          name: 'Bob',
+          outcome: 0,
+          password: 'typicalpassword',
+        }),
+      );
   });
 });
